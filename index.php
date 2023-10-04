1c93cba39416475fa6e1a60915afc66aec8e934b
Mathis <nicolas.mathis3@gmail.com>
2023-10-04 15:51:19 +0200

    MonBlog - Version 1

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //Accès aux données
-$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'testblog', '12345');
+$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'testblog', '12345');
 $billets = $bdd->query('select idBillet as id, dateBillet as date,'
     . ' titreBillet as titre, contenuBillet as contenu from BILLET'
     . ' order by idBillet desc');
